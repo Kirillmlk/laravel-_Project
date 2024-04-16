5641b97b1c6377ecd280771450126ad01d965459
Kirillmlk <kostakov.kiirill@yandex.by>
2024-04-16 22:57:29 +0300

    add routes

---
+++ resources/views/about.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+
+@section('main_content')
+    <h1>Страница О Нас</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga ipsam nihil, quidem quod repellat voluptates? Amet commodi earum error maiores repellendus vel. Dignissimos eligendi esse, minus optio quae quisquam vel!</p>
+
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layout')
+
+@section('main_content')
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga ipsam nihil, quidem quod repellat voluptates? Amet commodi earum error maiores repellendus vel. Dignissimos eligendi esse, minus optio quae quisquam vel!</p>
+@endsection
