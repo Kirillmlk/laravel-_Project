9cd1935b3ef411bc744792f5cea9c86819675444
Kirillmlk <kostakov.kiirill@yandex.by>
2024-04-21 22:08:51 +0300

    add db save

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [MainController::class, 'home']);
 Route::get('/about',  [MainController::class, 'about'] );
-Route::get('/review',  [MainController::class, 'review'] );
+Route::get('/review',  [MainController::class, 'review'] )->name('review');
 Route::post('/review/check',  [MainController::class, 'review_check'] );
 //Route::get('/user/{id}/{name}', function ($id, $name) {
 //    return 'ID: '. $id. '. Name: '.$name;
